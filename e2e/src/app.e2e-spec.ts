fb4d209ef5c41fd65c7634332b9dea900f12af65
diegominelli <diego.minelli@hotmail.com>
2020-10-05 18:58:40 -0300

    Criação do modulo conversor

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,23 +1,9 @@
-import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+import { ConversorDeMoedasPage } from './app.po';
 
-describe('workspace-project App', () => {
-  let page: AppPage;
+describe('conversor-de-moedas App', () => {
+  let page: ConversorDeMoedasPage;
 
   beforeEach(() => {
-    page = new AppPage();
-  });
-
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('conversor-de-moedas app is running!');
-  });
-
-  afterEach(async () => {
-    // Assert that there are no errors emitted from the browser
-    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-    expect(logs).not.toContain(jasmine.objectContaining({
-      level: logging.Level.SEVERE,
-    } as logging.Entry));
+    page = new ConversorDeMoedasPage();
   });
 });
