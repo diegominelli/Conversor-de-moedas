abbfc57d605ab0c57cd7888287a66b12025c5fbf
diegominelli <diego.minelli@hotmail.com>
2020-10-06 16:23:55 -0300

    Criando o componente modal de

--- src/app/conversor/conversor.module.ts
+++ src/app/conversor/conversor.module.ts
@@ -6,6 +6,7 @@ import { FormsModule } from '@angular/forms'
 import { ConversorComponent } from './components';
 import { MoedaService, ConversorService } from './services';
 import { NumeroDirective } from './directives';
+import { ModalCotacaoComponent } from './utils';
 
 
 @NgModule({
@@ -16,7 +17,8 @@ import { NumeroDirective } from './directives';
   ],
   declarations: [
     ConversorComponent,
-    NumeroDirective
+    NumeroDirective,
+    ModalCotacaoComponent
   ],
   exports: [
     ConversorComponent
